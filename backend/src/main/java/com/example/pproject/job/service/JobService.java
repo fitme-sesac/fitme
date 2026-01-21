95be7fa91f9f897fb74f222a727ec339286b6f1b
{danmaril} <dn.kr.ri.su@gmail.com>
2026-01-21 17:52:52 +0900

    지원자와 채용공고 매칭 관련 프로세스 구축

--- backend/src/main/java/com/example/pproject/job/service/JobService.java
+++ backend/src/main/java/com/example/pproject/job/service/JobService.java
@@ -19,7 +19,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
