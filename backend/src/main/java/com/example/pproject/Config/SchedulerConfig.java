fafe109faac3cc9f5d21d036937ebfc1697dba29
moonwonkim-a <happynei200@gmail.com>
2026-01-28 13:07:57 +0900

    feat : springboot batch, shedLock 설정

---
+++ backend/src/main/java/com/example/pproject/Config/SchedulerConfig.java
@@ -0,0 +1,20 @@
+package com.example.pproject.Config;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.redis.spring.RedisLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "10m")
+public class SchedulerConfig {
+
+    @Bean
+    public LockProvider lockProvider(RedisConnectionFactory connectionFactory) {
+        return new RedisLockProvider(connectionFactory, "shedlock");
+    }
+}
